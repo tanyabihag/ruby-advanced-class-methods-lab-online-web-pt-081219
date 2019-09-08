0d2f535e778e335f1a7961b1220413161c803a91
Tanya Bihag <tbihag@gmail.com>
2019-09-08 23:39:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,14 +55,18 @@ class Song
       song
     end  
     
-    def self.create_from_filename(filename)
-      result = self.new_from_filename(filename)
-      song = self.create
-      result.name = song.name
-      self.artist_name = result.artist_name
-      song
-    end
+     def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
   end
+
+  def self.destroy_all
+    self.all.clear
+  end
+end
     
     
     

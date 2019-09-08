2e953bf15b23b18b6401ddd19273fc044ab62fa4
Tanya Bihag <tbihag@gmail.com>
2019-09-08 23:23:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,39 @@ class Song
   end
     
     def self.new_from_filename(title)
+      song_array = filename.split(" - ")
+      song_array[1] = song_array[1].chomp(."mp3")
+      song = self.new 
+      song.name = song_array[1]
+      song.artist_name = song_array[0]
+      song
       
   end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 

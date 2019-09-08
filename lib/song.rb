e54bb1ecf9369847eb3beb65a75bd023b4ccbc0e
Tanya Bihag <tbihag@gmail.com>
2019-09-08 22:53:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,8 +27,10 @@ class Song
     song
   end
   
-  def self.find_by_name
+  def self.find_by_name(title)
+    result = self.all.detect {|song| song.name == title}
+    result
+  end
     
     
 end
-
